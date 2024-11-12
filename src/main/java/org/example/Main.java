da53538cdb2f1e5aeecf994cf801bdf9b55f9d41
khalvashi298 <khalvashim2@gmail.com>
2024-11-12 15:33:41 +0400

    switch/case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,26 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        int currentAge=12;
-        int minAGE= 24;
+       int studentscore=100;
 
-        String S= (currentAge>minAGE)? "Tru" : "false";
-        System.out.println(S);
+       switch (studentscore) {
+           case 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100:
+               System.out.println("A");
+               break;
+
+            case 80, 81, 82,  83, 84, 85, 86, 87, 88, 89 :
+                System.out.println("B");
+                break;
+
+          case 70, 71,72,73,74,75,76,77,78,79 :
+              System.out.println("C");
+              break;
+
+           default:
+               System.out.println("NO");
+               break;
+      }
+
 
 
     }
