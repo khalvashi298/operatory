fee18a61cea491438ab0b3669b829242b627baa1
khalvashi298 <khalvashim2@gmail.com>
2024-11-12 14:22:06 +0400

    oper

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int currentAge=12;
+        int minAGE= 24;
+
+        String S= (currentAge>minAGE)? "Tru" : "false";
+        System.out.println(S);
+
+
+    }
+
+}
